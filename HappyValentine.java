8011198f5dbc662da861c5e494e0ec286b68b025
aidelojep <57565189+aidelojep@users.noreply.github.com>
2021-02-18 21:15:43 +0100

    Add files via upload

---
+++ HappyValentine.java
@@ -0,0 +1,34 @@
+public class HappyValentine
+{public static void main (String....args)
+{
+ System.out.println                      (* * *                        * * * );
+System.out.println                     (* * *  * *                  *  *  * *);
+System.out.println                    (* * * * *  *               * * * * * *) ;         
+System.out.println                      (* * *  *  *  *        * * *  * * *);
+System.out.println                       (* * * * * *  * * * * * * * * * *);
+ System.out.println                       (*  *  *   *  * ****  * * * * *);
+   System.out.println                       (*  * * * *  *  *  *  *  * *);
+   System.out.println                         ( * * * * * * * * * * * *);
+    System.out.println                           (* * * * * * * * * *);
+    System.out.println                            ( * * * * * * * * *);
+    System.out.println                              (* * * * * * * *);
+     System.out.println                               (* * * * * *);
+     System.out.println                                 ( * * * *);
+      System.out.println                                  ( * * );
+      System.out.println                                    (**);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+}
